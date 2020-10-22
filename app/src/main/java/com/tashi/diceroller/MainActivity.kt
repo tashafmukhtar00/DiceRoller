011d48cf49dc0519a21da40f0b665af0993bab6f
tashi <tashafmukhtar@gmail.com>
2020-10-22 17:42:51 +0400

    final app

--- app/src/main/java/com/tashi/diceroller/MainActivity.kt
+++ app/src/main/java/com/tashi/diceroller/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.tashi.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rollButton: Button
+    private lateinit var resultImage : ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rollButton = findViewById(R.id.rollButton)
+        resultImage  = findViewById(R.id.resultImage);
+
+
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+
+
+        var id = when(Random.nextInt(6) +1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+
+           else -> R.drawable.empty_dice
+       }
+
+        resultImage.setImageResource(id)
     }
 }
